e061a2d59bdc9a6ac065d8f625c69f7c98939712
turttlehouse <builandrun464@gmail.com>
2026-07-24 08:11:59 +0545

    displayed movie counts by genre in admin dashboard

--- src/page/admin/Dashboard.jsx
+++ src/page/admin/Dashboard.jsx
@@ -3,33 +3,27 @@ import { MovieContext } from "../../context/MovieContext";
 
 const Dashboard = () => {
   const { movies } = useContext(MovieContext);
+  // console.log(movies);
   const totalMovies = movies.length;
-  console.log(totalMovies);
+  // console.log(totalMovies);
 
-  // reduce() => method js => collection of values=> single value
-  // group movies by genre
-
-  // acc = {} = emptyObject
-  // currValue
-  //   {
-  //     name: 'Jaari',
-  //     poster:
-  //       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSJxsAltHtgJleSohbhSaeR7mlVYUkKjF71a6Ask0t3Q&s=10',
-  //     rating: '3',
-  //     genre: 'Love',
-  //     id: '2'
-  //   },
-
-  
-  // const groupedMoviesByGenre = movies?.reduce((acc, currValue) => {
-  //   const genre = currValue?.genre;
-  //   if (!acc[genre]) {
-  //     acc[genre] = [];
-  //   }
-  //   acc[genre].push(currValue);
-  // }, {});
+  // movies.reduce(fn,initialValue)
+  const groupedMoviesByGenre = movies.reduce((acc, currValue) => {
+    let genre = currValue.genre;
+    // console.log(genre);
+    if (!acc[genre]) {
+      acc[genre] = [];
+    }
+    acc[genre].push(currValue);
+    return acc;
+  }, {});
 
   // console.log(groupedMoviesByGenre);
+
+  // {
+  //   'action' : [dhurandar],
+  //   'sci-fi' : [avatar]
+  // }
 
   return (
     <div className="bg-green-50 dark:bg-gray-800 pt-12 sm:pt-16">
@@ -54,22 +48,21 @@ const Dashboard = () => {
                     <span>{totalMovies}</span>+
                   </dd>
                 </div>
-                <div className="flex flex-col border-t border-b border-gray-100 dark:border-gray-700 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-                  <dt className="order-2 mt-2 text-lg font-normal leading-6 text-gray-500 dark:text-gray-300">
-                    Analytic Tools and Machine Learning Models
-                  </dt>
-                  <dd className="order-1 text-5xl font-bold tracking-tight text-green-600 dark:text-green-400">
-                    <span>100</span>+
-                  </dd>
-                </div>
-                <div className="flex flex-col border-t border-gray-100 dark:border-gray-700 p-6 text-center sm:border-0 sm:border-l">
-                  <dt className="order-2 mt-2 text-lg font-normal leading-6 text-gray-500 dark:text-gray-300">
-                    Real-time Data Streams and Dashboards
-                  </dt>
-                  <dd className="order-1 text-5xl font-bold tracking-tight text-green-600 dark:text-green-400">
-                    <span>10</span>+
-                  </dd>
-                </div>
+
+                {Object.entries(groupedMoviesByGenre).map(([genre,movie]) => {
+                  // console.log(genre)
+                  // console.log(movie)
+                  return (
+                    <div className="flex flex-col border-t border-b border-gray-100 dark:border-gray-700 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                      <dt className="order-2 mt-2 text-lg font-normal leading-6 text-gray-500 dark:text-gray-300">
+                        {genre}
+                      </dt>
+                      <dd className="order-1 text-5xl font-bold tracking-tight text-green-600 dark:text-green-400">
+                        <span>{movie.length}</span>+
+                      </dd>
+                    </div>
+                  );
+                })}
               </dl>
             </div>
           </div>
